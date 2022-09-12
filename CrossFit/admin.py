5e41168a7fbfc74ca743d757fd7fa2785e2311f5
[dmorang5] <[dmorang@unemi.edu.ec]>
2022-09-12 09:08:15 -0500

    final commit

--- CrossFit/admin.py
+++ CrossFit/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+
+import CrossFit
 from .models import Venta, Cliente, Accesorios, Producto
 admin.site.register(Venta)
 admin.site.register(Cliente)
 admin.site.register(Accesorios)
 admin.site.register(Producto)
 # Register your models here.
+
